cc778eb3de638b7fccd2ca51e37ea45979ee7539
Munazzar Khan <gex.18@hotmail.com>
2024-08-24 20:50:14 +0500

    few changes for host

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,12 +15,13 @@ import { AppController } from './app.controller';
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     TypeOrmModule.forRoot({
+      url: process.env.POSTGRES_URL,
       type: 'postgres',
-      host: process.env.DB_HOST,
+      host: process.env.POSTGRES_HOST,
       port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
