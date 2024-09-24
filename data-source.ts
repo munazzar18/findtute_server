13f8f10b623a098d8389211d8547998c790bf3a7
Munazzar Khan <gex.18@hotmail.com>
2024-09-24 21:04:10 +0500

    added config module in ts file

--- data-source.ts
+++ data-source.ts
@@ -3,6 +3,12 @@ import { SubjectsEntity } from "src/subjects/subjects.entity";
 import { UserEntity } from "src/user/user.entity";
 import { DataSource, DataSourceOptions } from "typeorm";
 
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot({
+    envFilePath: ['.env', '.env.development', 'env.production']
+})
+
 
 export const dataSourceOptions: DataSourceOptions = {
     type: 'postgres',
